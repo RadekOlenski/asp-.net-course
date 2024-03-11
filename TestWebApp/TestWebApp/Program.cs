b081449a290048598b1cd767a3a8549814d27c67
Radek Oleński <radek.olenski@somethingrandom.com>
2024-03-11 22:06:53 +0100

    [Added] Simple page logic reaction to path from http requests

--- TestWebApp/TestWebApp/Program.cs
+++ TestWebApp/TestWebApp/Program.cs
@@ -1,3 +1,5 @@
+using TestWebApp;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,7 +7,7 @@ var app = builder.Build();
 app.MapGet("/", static () => "Hello World!");
 
 app.Run(
-    async static context =>
+    static async context =>
     {
         //Putting own key/value pair into response headers
         context.Response.Headers["Test-Key"] = "Test Value";
@@ -13,9 +15,9 @@ app.Run(
         //Setting response content type
         context.Response.Headers.ContentType = "text/html";
 
-        //Awaiting async responses with HTML content
-        await context.Response.WriteAsync("<h1>Wrong site</h1>");
-        await context.Response.WriteAsync("<h2>Please try again</h2>");
+        await SimplePageLogic.ProcessLandingPage(context);
+        await SimplePageLogic.ProcessWrongPage(context);
+        await SimplePageLogic.ProcessExitPage(context);
     });
 
 app.Run();
---
+++ TestWebApp/TestWebApp/SimplePageLogic.cs
@@ -0,0 +1,39 @@
+﻿namespace TestWebApp;
+
+internal static class SimplePageLogic
+{
+    private const string ExitPagePath = "/exit";
+    private const string LandingPagePath = "/";
+
+    internal static Task ProcessExitPage(HttpContext context) =>
+        context.Request.Path != ExitPagePath
+            ? Task.CompletedTask
+            : context.Response.WriteAsync("<h1> Exiting page </h1>");
+
+    internal static async Task ProcessLandingPage(HttpContext context)
+    {
+        if (context.Request.Path != LandingPagePath)
+        {
+            return;
+        }
+
+        //Awaiting async responses with HTML content
+        await context.Response.WriteAsync("<h1>Welcome</h1>");
+        await context.Response.WriteAsync("<h2>This is simple ASP .NET site</h2>");
+        await context.Response.WriteAsync("<h3>Currently available content:</h3>");
+        await context.Response.WriteAsync("<h3> - Home</h3>");
+        await context.Response.WriteAsync("<h3> - Exit</h3>");
+    }
+
+    internal static async Task ProcessWrongPage(HttpContext context)
+    {
+        if (context.Request.Path == LandingPagePath || context.Request.Path == ExitPagePath)
+        {
+            return;
+        }
+
+        //Awaiting async responses with HTML content
+        await context.Response.WriteAsync("<h1>Wrong site</h1>");
+        await context.Response.WriteAsync("<h2>Please try again</h2>");
+    }
+}
