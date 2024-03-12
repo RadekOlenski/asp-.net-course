d0a3bb2f65ec7082c6a951c3e174ddb36139189d
Radek Oleński <radek.olenski@somethingrandom.com>
2024-03-12 09:25:24 +0100

    [Added] Query string example to exit page

--- TestWebApp/TestWebApp/PageLogic/SimplePageLogic.cs
+++ TestWebApp/TestWebApp/PageLogic/SimplePageLogic.cs
@@ -14,10 +14,17 @@ internal sealed class SimplePageLogic : IPageLogic
 
     public static IPageLogic CreateInstance() => new SimplePageLogic();
 
-    public Task ProcessExitPage(HttpContext context) =>
-        context.Request.Path != ExitPagePath
-            ? Task.CompletedTask
-            : context.Response.WriteAsync("<h1> Exiting page </h1>");
+    public async Task ProcessExitPage(HttpContext context)
+    {
+        if (context.Request.Path != ExitPagePath)
+        {
+            return;
+        }
+
+        await context.Response.WriteAsync("<h1> Exiting page </h1>");
+        await LookForUserID(context);
+        await LookForUserBrowser(context);
+    }
 
     public async Task ProcessLandingPage(HttpContext context)
     {
@@ -36,14 +43,40 @@ internal sealed class SimplePageLogic : IPageLogic
 
     public async Task ProcessWrongPage(HttpContext context)
     {
-        if (Array.Exists(_availablePages, pageName => pageName == context.Request.Path))
+        var requestedPath = context.Request.Path;
+        if (Array.Exists(_availablePages, pageName => pageName == requestedPath))
         {
             return;
         }
 
         //Awaiting async responses with HTML content
-        await context.Response.WriteAsync("<h1>Wrong site</h1>");
-        await context.Response.WriteAsync("<h2>Please try again</h2>");
+        await context.Response.WriteAsync("<h1>Wrong site!</h1>");
+        await context.Response.WriteAsync($"<h2>Site {requestedPath} does not exist.</h2>");
+        await context.Response.WriteAsync("<h2>Please try again.</h2>");
+    }
+
+    private static async Task LookForUserBrowser(HttpContext context)
+    {
+        if (context.Request.Headers.TryGetValue("User-Agent", out var userBrowser))
+        {
+            await context.Response.WriteAsync($"<h2> User using browser: {userBrowser} </h2>");
+        }
+        else
+        {
+            await context.Response.WriteAsync("<h2> Browser could not be determined. </h2>");
+        }
+    }
+
+    private static async Task LookForUserID(HttpContext context)
+    {
+        if (context.Request.Query.TryGetValue("id", out var idValue))
+        {
+            await context.Response.WriteAsync($"<h2> Logging out user with id: {idValue} </h2>");
+        }
+        else
+        {
+            await context.Response.WriteAsync("<h2> No user found. </h2>");
+        }
     }
 
     private void ConstructPagesArray(params string[] availablePages)
